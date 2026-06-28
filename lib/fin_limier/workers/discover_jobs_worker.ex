ded33045fd18dcee8890bf006d07eb37e0369a8a
Mathieu Cizo <maciz@outlook.fr>
2026-06-28 23:44:24 +0200

    Log failures from job discovery worker

--- lib/fin_limier/workers/discover_jobs_worker.ex
+++ lib/fin_limier/workers/discover_jobs_worker.ex
@@ -5,13 +5,25 @@ defmodule FinLimier.Workers.DiscoverJobsWorker do
 
   use Oban.Worker, queue: :discovery, max_attempts: 5
 
+  require Logger
+
   alias FinLimier.UseCases.DiscoverJobs
 
   @impl Oban.Worker
   def perform(%Oban.Job{}) do
     case DiscoverJobs.run() do
-      {:ok, _summary} -> :ok
-      {:error, reason} -> {:error, reason}
+      {:ok, summary} ->
+        log_failures(summary)
+        :ok
+
+      {:error, reason} ->
+        {:error, reason}
     end
+  end
+
+  defp log_failures(%{failures: []}), do: :ok
+
+  defp log_failures(%{failures: failures}) do
+    Logger.warning("Job discovery completed with #{length(failures)} offer failure(s): #{inspect(failures)}")
   end
 end
